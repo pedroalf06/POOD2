0977529bdb4a6df7692565686ec2593f1c8cc8a2
ALUMNO# <ALUMNO#@ALUMNO-28>
2025-08-21 09:52:09 -0500

    ESTA ES MI PRIMER CARGA DEL PROYECTO

---
+++ taller1/Taller1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pood2.taller1;
+
+/**
+ *
+ * @author ALUMNO#
+ */
+public class Taller1 {
+
+    public static void main(String[] args) {
+        
+        int numero1 = 10, numero2 = 25, suma;
+        
+        suma = numero1 + numero2;
+        
+        System.out.println("Suma = "+ suma);
+    }
+}
